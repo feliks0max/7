be84fb5de6d4a902025cb273bca95f416a14286b
feliks0max <67854949+feliks0max@users.noreply.github.com>
2020-07-05 10:33:05 +0300

    Надо

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+	'TOKEN': 'NzI4NjQ1OTI5NDU2MTA3NTUx.Xv9aeg.jpwAu5lzhkL-V-KAe8zUePa3qbU',
+	'NAME BOT': 'ArtemBot',
+	'ID': 719135635272761375,
+	'PREFIX': '/',
+	'OWNER' : 'Артём 13327#7762',
+	'data_create' : '15.05.2020'
+}
